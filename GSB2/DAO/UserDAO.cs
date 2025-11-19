84e3542da11d66eea105c0fa0ad46ea8053b2f44
Safwane <safwaneb1@gmail.com>
2025-11-19 08:23:25 +0100

    Premier push depuis ce PC

---
+++ GSB2/DAO/UserDAO.cs
@@ -0,0 +1,56 @@
+﻿using GSB2.Models;
+using Microsoft.VisualBasic.ApplicationServices;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GSB2.DAO
+{
+    internal class UserDAO
+    {
+        private readonly Database db = new Database();
+            public Users GetUsers(string email,string password)
+            {
+                using(var connection = db.GetConnection())
+                {
+                    try
+                    {
+                        connection.Open();
+                        // create a MySQL command and set the SQL statement with parameters
+                        MySqlCommand myCommand = new MySqlCommand();
+                        myCommand.Connection = connection;
+                        myCommand.CommandText = @"SELECT * FROM users WHERE email = @email AND password = SHA2(@password, 256);";
+                        myCommand.Parameters.AddWithValue("@email", email);
+                        myCommand.Parameters.AddWithValue("@password", password);
+
+                        using var myReader = myCommand.ExecuteReader();
+                        if (myReader.Read())
+                        {
+                            int id_Users = myReader.GetInt32("id_Users");
+                            string name = myReader.GetString("name");
+                            string firstname = myReader.GetString("firstname");
+                            bool role = myReader.GetBoolean("role");
+                            connection.Close();
+                            return new Users(id_Users, name, firstname, email, role);
+                        }
+
+                        connection.Close();
+                        return null;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Erreur de connexion : {ex.Message}", "Erreur",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return null;
+                    }
+            }
+        }
+    }
+}
---
+++ GSB2/Models/Patients.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace GSB2.Models
+{
+    public class Patients
+    {
+
+
+        // ceci est une propriété permet de lire et d'écrire les attribue de la classe
+        public int Id_Patients { get; set; }
+        public int Id_Users { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Firstname { get; set; }
+        public bool Gender { get; set; }
+
+
+        // ceci est le constructeur, permet d'accédr aux méthode et propriété de notre class
+        public Patients()
+        {
+
+        }
+
+        // ceci est une surcharge du constructeur elle permer la création d'un objet user avec les champ passé en param
+        public Patients(int id_patients, int id_users, string name, string age, string firstname, bool gender)
+        {
+            Id_Users = id_users;
+            Id_Patients = id_patients;
+            Name = name;
+            Age = age;
+            Firstname = firstname;
+            Gender = gender;
+        }
+    }
+}
